a330ef1ec97f075655d69e52b6e29ba66f080eda
VirajYR <virajrao665@gmail.com>
2024-09-02 18:45:29 +0530

    Bugs Fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Admin from "./Pages/Admin/Admin"
+import AddProduct from './Components/AddProduct/AddProduct'
 
 const App = () => {
   return (
     <>
     <Navbar/>
     <Admin/>
+    <AddProduct/>
     </>
   )
 }
